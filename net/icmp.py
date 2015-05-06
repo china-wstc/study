695e1ca048fc9a570612dfafd4d6f4c20e5b752e
Wang Linggui <wanglinggui@gmail.com>
2015-05-06 15:53:35 -0700

    icmp

--- net/icmp.py
+++ net/icmp.py
@@ -2,6 +2,7 @@
 
 import struct
 
+
 class ICMP(object):
   def __init__(self):
     # 1Byte 类型
@@ -20,11 +21,27 @@ class ICMP(object):
                                  'i_checksum: %s' % hex(self.i_checksum)))
 
 
+  def checksum(self, buff):
+    sum = 0
+    for num in struct.unpack('>%sH' % (len(buff) / 2), buff):
+      sum += (~num) & 0xFFFF
+      sum = (sum >> 16) + sum & 0xFFFF
+    return sum & 0xFFFF
+
+
   def unpack(self, buff):
     self.i_type, self.i_code, self.i_checksum = \
         struct.unpack('>BBH', buff[:4])
     return
 
+  def _pack(self):
+    return ''
+
+  def pack(self):
+    self.i_checksum = 0
+    buff = self._pack()
+    print '----------', len(buff), self.checksum(buff)
+    return buff[:2] + struct.pack('>H', self.checksum(buff)) + buff[4:]
 
 
 class IcmpAddrMask(ICMP):
@@ -37,48 +54,66 @@ class IcmpAddrMask(ICMP):
     self.i_mask = 0
     return
 
-
-  def _checksum(self):
-    def _s(num):
-      return (~num) & 0xFFFF
-
-    # little endian
-    csum = _s(self.i_type << 8)
-    csum += _s(self.i_checksum)
-
-    csum = (csum >> 16) + (csum & 0xFFFF)
-    csum += _s(self.i_token >> 16)
-
-    csum = (csum >> 16) + (csum & 0xFFFF)
-    csum += _s(self.i_token & 0xFFFF)
-
-    csum = (csum >> 16) + (csum & 0xFFFF)
-
-    csum += _s(self.i_mask >> 16)
-    csum = (csum >> 16) + (csum & 0xFFFF)
-
-    csum += _s(self.i_mask & 0xFFFF)
-    csum = (csum >> 16) + (csum & 0xFFFF)
-    return csum
-
   def _pack(self):
-    buff = self._pack()
-    csum = 0
-    for num in struct.unpack('>%sH' % (len(buff) / 2), buff):
-      csum += num
-      csum = (csum >> 16) + (csum & 0xFFFF)
-    csum = (~csum) & 0xFFFF
-
-    print 'check sum2 ', csum
-    return
-
-  def pack(self):
-    self.i_checksum = self._checksum()
-    print 'check sum ', self._checksum()
+    self.i_checksum = 0
     return struct.pack('>BBHII', self.i_type, self.i_code, self.i_checksum, self.i_token, self.i_mask)
 
   def unpack(self, buff):
     self.i_type, self.i_code, self.i_checksum, self.i_token, self.i_mask = \
         struct.unpack('>BBHII', buff)
+    return
+
+class IcmpFetchTime(ICMP):
+  def __init__(self):
+    super(IcmpFetchTime, self).__init__()
+
+    # 2 Byte 标识符
+    self.i_mark = 0
+    # 2 Byte 序列号
+    self.i_seqid = 0
+
+    # 4 Byte 发起时间
+    self.i_origt = 0
+
+    # 4 Byte 接收时间
+    self.i_recvt = 0
+
+    # 4 Byte 传送时间
+    self.i_sendt = 0
+
+  def __str__(self):
+    return 'IcmpFetchTime ' + \
+        ', '.join(('i_type: %d' % self.i_type,
+                   'i_code: %d' % self.i_code,
+                   'i_checksum: %s' % hex(self.i_checksum),
+                   'i_mark: %d' % self.i_mark,
+                   'i_seqid: %d' % self.i_seqid,
+                   'i_origt: %d' % self.i_origt,
+                   'i_recvt: %d' % self.i_recvt,
+                   'i_sendt: %d' % self.i_sendt,
+                   ))
+
+  def _pack(self):
+    self.i_checksum = 0
+    return struct.pack('>BBH2H3I',
+        self.i_type,
+        self.i_code,
+        self.i_checksum,
+        self.i_mark,
+        self.i_seqid,
+        self.i_origt,
+        self.i_recvt,
+        self.i_sendt
+        )
+
+  def unpack(self, buff):
+    self.i_type, \
+    self.i_code, \
+    self.i_checksum, \
+    self.i_mark, \
+    self.i_seqid, \
+    self.i_origt, \
+    self.i_recvt, \
+    self.i_sendt = struct.unpack('>BBH2H3I', buff)
     return
 
--- net/icmpFetchTime.py
+++ net/icmpFetchTime.py
@@ -8,6 +8,7 @@ TCP/IP详解 卷1 6.3 Page52
 import socket
 import icmp
 import time
+import os
 
 def main():
   sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
@@ -15,23 +16,25 @@ def main():
   sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
   callid = 1
   while True:
-    iam = icmp.IcmpAddrMask()
-    iam.i_type = 17
-    iam.i_token = callid
-    iam.i_mask = 0
+    iam = icmp.IcmpFetchTime()
+    iam.i_type = 13
+    iam.i_seqid = os.getpid()
+    iam.i_mark  = 2
+    iam.i_origt = 30000
 
 
-    sock.sendto(iam.pack(), ("192.168.1.255", 1))
+    buff = iam.pack()
+
+    print 'send ', len(buff), hex(iam.checksum(buff))
+    sock.sendto(buff, ("192.168.1.255", 0))
 
     data, addr = sock.recvfrom(2048)
 
-    print len(data)
+    iam = icmp.IcmpFetchTime()
 
-    iam = icmp.IcmpAddrMask()
+    print addr, len(data), hex(iam.checksum(data[20:]))
     iam.unpack(data[20:])
-
-    print addr, iam
-
+    print addr, len(data), iam
     time.sleep(10)
 
 main()
