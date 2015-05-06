10277d51e4b9af40c23867f2f5cb1c5984c949d8
Wang Linggui <wanglinggui@gmail.com>
2015-05-06 15:09:29 -0700

    icmp

---
+++ net/icmp.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+import struct
+
+class ICMP(object):
+  def __init__(self):
+    # 1Byte 类型
+    self.i_type = 0
+
+    # 1Byte 代码
+    self.i_code = 0
+
+    # 2Byte
+    self.i_checksum = 0
+
+
+  def __str__(self):
+    return 'ICMP: ' + ', '.join(('i_type: %d' % self.i_type,
+                                 'i_code: %d' % self.i_code,
+                                 'i_checksum: %s' % hex(self.i_checksum)))
+
+  def unpack(self, buff):
+    self.i_type, self.i_code, self.i_checksum = \
+        struct.unpack('>BBH', buff[:4])
+    return
+
+
+
+class IcmpAddrMask(ICMP):
+  def __init__(self):
+    super(IcmpAddrMask, self).__init__()
+    # 4Byte [2Byte标识符、2Byte序列号], 其实就是一个rpc的token
+    self.i_token = 0
+
+    # 4Byte 掩码
+    self.i_mask = 0
+    return
+
+
+  def _checksum(self):
+    def _s(num):
+      return (~num) & 0xFFFF
+
+    # little endian
+    csum = _s(self.i_type | (self.i_code << 8))
+
+    csum +=  _s(self.i_checksum)
+    csum = (csum >> 16) + (csum & 0xFFFF)
+
+    csum += _s(self.i_token >> 16)
+
+    csum = (csum >> 16) + (csum & 0xFFFF)
+
+    csum += _s(self.i_token & 0xFFFF)
+
+    csum = (csum >> 16) + (csum & 0xFFFF)
+
+    csum += _s(self.i_mask >> 16)
+
+    csum = (csum >> 16) + (csum & 0xFFFF)
+
+    csum += _s(self.i_mask & 0xFFFF)
+
+    csum = (csum >> 16) + (csum & 0xFFFF)
+    return csum
+
+  def pack(self):
+    self.i_checksum = self._checksum()
+    return struct.pack('>BBHII', self.i_type, self.i_code, self.i_checksum, self.i_token, self.i_mask)
+
+  def unpack(self, buff):
+    self.i_type, self.i_code, self.i_checksum, self.i_token, self.i_mask = \
+        struct.unpack('>BBHII', buff)
+    return
+
---
+++ net/icmpaddrmask.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+'''
+TCP/IP详解 卷1 6.3 Page52
+'''
+
+
+import socket
+import icmp
+import time
+
+def main():
+  sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
+
+  callid = 1
+  while True:
+    iam = icmp.IcmpAddrMask()
+    iam.i_type = 17
+    iam.i_token = callid
+    iam.i_mask = 0
+
+    iam.i_checksum = iam._checksum()
+    print iam, hex(iam._checksum())
+
+    sock.sendto(iam.pack(), ("192.168.1.200", 0))
+
+    data, addr = sock.recvfrom(2048)
+
+    iam = icmp.IcmpAddrMask()
+    iam.unpack(data[:12])
+
+    print addr, len(data), iam, hex(iam._checksum())
+
+    time.sleep(10)
+
+main()
+
